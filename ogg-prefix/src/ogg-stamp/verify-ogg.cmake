4933b9512bcddf94d4293ada52a7a30db974b76d
Sylvestr Lozynskyy <pan.totoro@gmail.com>
2018-02-10 19:37:03 +0200

    oops

--- ogg-prefix/src/ogg-stamp/verify-ogg.cmake
+++
@@ -1,4 +0,0 @@
-set(file "/home/midnightas/cpp/pizzajam/ogg-prefix/src/libogg-1.3.1.tar.gz")
-message(STATUS "verifying file...
-     file='${file}'")
-message(STATUS "verifying file... warning: did not verify file - no URL_HASH specified?")
